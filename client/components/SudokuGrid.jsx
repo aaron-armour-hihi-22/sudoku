0a8699d3d8a545fcd909eefde869ce4f6af8bba9
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2022-12-20 16:39:42 +1300

    Create the full Sudoku grid in a single table

--- client/components/SudokuGrid.jsx
+++ client/components/SudokuGrid.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
 
-function createRow(row) {
+const cellIndices = [
+  [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  [9, 10, 11, 12, 13, 14, 15, 16, 17],
+  [18, 19, 20, 21, 22, 23, 24, 25, 26],
+  [27, 28, 29, 30, 31, 32, 33, 34, 35],
+  [36, 37, 38, 39, 40, 41, 42, 43, 44],
+  [45, 46, 47, 48, 49, 50, 51, 52, 53],
+  [54, 55, 56, 57, 58, 59, 60, 61, 62],
+  [63, 64, 65, 66, 67, 68, 69, 70, 71],
+  [72, 73, 74, 75, 76, 77, 78, 79, 80],
+]
+
+function createCellRow(row, index) {
   return (
-    <>
+    <tr key={index}>
       {row.map((element) => (
         <td key={element}>{element}</td>
       ))}
-    </>
+    </tr>
   )
 }
 
-function createSubSquares() {
-  const subSquares = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-  ]
-
-  return (
-    <>
-      {subSquares.map((row, index) => (
-        <tr key={index}>{createRow(row)}</tr>
-      ))}
-    </>
-  )
+function createCells() {
+  return cellIndices.map((row, index) => createCellRow(row, index))
 }
 
 function SudokuGrid() {
   return (
     <>
-      {/* <p>This is the grid!</p> */}
-      <table>{createSubSquares()}</table>
+      <table>{createCells()}</table>
     </>
   )
 }
