bfeed8633447fcb6bd341687c1f1849f462ac552
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2022-12-30 18:38:30 +1300

    Add reducer cases for RESET_GRID

--- client/reducers/selectedCellIndex.js
+++ client/reducers/selectedCellIndex.js
@@ -1,3 +1,4 @@
+import { RESET_GRID } from '../actions/cells'
 import {
   SET_SELECTED_CELL,
   CLEAR_SELECTED_CELL,
@@ -9,6 +10,9 @@ function reducer(state = initialState, action) {
   const { type, payload } = action
 
   switch (type) {
+    case RESET_GRID:
+      return null
+
     case SET_SELECTED_CELL:
       return payload
 
