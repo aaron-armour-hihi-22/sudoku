9bf11da34e5596eff76b833bc9e4d8df299faa31
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-01-19 20:19:00 +1300

    Remove the SudokuOptions component as the modal now does this job

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import MenuAppBar from './MenuAppBar'
 import NewSudokuModal from './NewSudokuModal'
 import SudokuGrid from './SudokuGrid'
-import SudokuOptions from './SudokuOptions'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <NewSudokuModal />
       <section className="main">
         <SudokuGrid />
-        <SudokuOptions />
       </section>
     </>
   )
