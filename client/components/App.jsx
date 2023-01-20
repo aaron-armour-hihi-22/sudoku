3eefbbd508670fac550305be017dd2978f4bde3c
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-01-20 19:07:39 +1300

    Add the SnackMessage component in to the App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import MenuAppBar from './MenuAppBar'
 import NewSudokuModal from './NewSudokuModal'
+import SnackMessage from './SnackMessage'
 import SudokuGrid from './SudokuGrid'
 
 function App() {
   return (
     <>
       <MenuAppBar />
+      <SnackMessage />
       <NewSudokuModal />
       <section className="main">
         <SudokuGrid />
